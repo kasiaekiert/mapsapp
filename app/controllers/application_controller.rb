65b9a95a9212464b97305740f7cee4f85e79e8f1
Kasia <kasia.ekiert@gmail.com>
2019-10-07 16:19:57 +0200

    first translations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  around_action :switch_locale     
 
+  def switch_locale(&action)     
+    # I18n.with_locale(:pl, &action)
+    locale = params[:locale] || I18n.default_locale     
+    I18n.with_locale(:pl, &action)   
+  end
 end
