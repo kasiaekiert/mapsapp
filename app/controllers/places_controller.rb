d96b14cedfea6864cee6720e10192232d442c83b
Kasia <kasia.ekiert@gmail.com>
2019-10-08 12:41:35 +0200

    views translations

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -29,7 +29,7 @@ class PlacesController < ApplicationController
 
     respond_to do |format|
       if @place.save
-        format.html { redirect_to @place, notice: 'Place was successfully created.' }
+        format.html { redirect_to @place, notice: t('.places_create')}
         format.json { render :show, status: :created, location: @place }
       else
         format.html { render :new }
@@ -43,7 +43,7 @@ class PlacesController < ApplicationController
   def update
     respond_to do |format|
       if @place.update(place_params)
-        format.html { redirect_to @place, notice: 'Place was successfully updated.' }
+        format.html { redirect_to @place, notice: t('.places_update') }
         format.json { render :show, status: :ok, location: @place }
       else
         format.html { render :edit }
@@ -57,7 +57,7 @@ class PlacesController < ApplicationController
   def destroy
     @place.destroy
     respond_to do |format|
-      format.html { redirect_to places_url, notice: 'Place was successfully destroyed.' }
+      format.html { redirect_to places_url, notice: t('.places_destroy') }
       format.json { head :no_content }
     end
   end
