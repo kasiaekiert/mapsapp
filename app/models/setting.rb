3de55bbe2f38abd4a4f033edb1253e658ef511b2
Kasia <kasia.ekiert@gmail.com>
2019-10-17 18:06:49 +0200

    remove useless validation for locale

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -3,8 +3,6 @@ class Setting < ApplicationRecord
 
   enum locale: [:en, :pl]
 
-  validates :locale, inclusion: { in: ['en', 'pl']}
-
   def deafault_locale
     if current_user.setting == nil
       config.i18n.default_locale = :pl
