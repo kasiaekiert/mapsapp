0a696239da8402d58e8e9eb744175c2834cc614d
Kasia <kasia.ekiert@gmail.com>
2019-10-17 18:01:53 +0200

    gem after_party to set up setting for all users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def switch_locale(&action)     
-    locale = if current_user
-      current_user.setting.locale || I18n.default_locale 
-    else
-      I18n.default_locale
-    end
+    locale = current_user ? current_user.setting.locale : I18n.default_locale
 
     I18n.with_locale(locale, &action)   
   end
--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,6 +2,6 @@ class ProfileController < ApplicationController
 
   def show
     @user = current_user
-    @setting = @user.setting || Setting.create(user: @user, locale: :en)
+    @setting = @user.setting
   end
 end
