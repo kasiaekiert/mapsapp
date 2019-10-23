c9a1daaf30cd5ab9ed6a6691c0bf7c48833c85ad
Kasia <kasia.ekiert@gmail.com>
2019-10-23 19:38:27 +0200

    custom validator

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ class ApplicationController < ActionController::Base
 
   def switch_locale(&action)     
     locale = current_user ? current_user.setting.locale : I18n.default_locale
-
+  
     I18n.with_locale(locale, &action)   
+
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :place
   belongs_to :user
+
+  validates :name, name_length: { greater_than: 8 }
 end
