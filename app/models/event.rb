e86410f5f5107231227385eb6c269072bc603dfe
Kasia <kasia.ekiert@gmail.com>
2019-10-31 18:08:58 +0100

    members - user

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :place
-  has_and_belongs_to_many :users
+  has_and_belongs_to_many :members, class_name: "User"
   belongs_to :user
 
   validates :name, name_lenght: { minimum: 5 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_and_belongs_to_many :events
+  has_many :owned_events, class_name: 'Event'
   has_one :setting
 
 end
