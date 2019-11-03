495e1230f0ed24179935fcfe26d3cc5b9e5aa515
Kasia <kasia.ekiert@gmail.com>
2019-11-03 17:15:10 +0100

    members, user, delete validator

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,5 @@
 class Event < ApplicationRecord
-  belongs_to :place
-  has_and_belongs_to_many :members, class_name: "User"
+  belongs_to :place 
   belongs_to :user
-
-  validates :name, name_lenght: { minimum: 5 }
+  has_and_belongs_to_many :members, class_name: "User"
 end
--- app/validators/name_lenght_validator.rb
+++
@@ -1,9 +0,0 @@
-class NameLenghtValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    return unless options.key?(:minimum)
-    name_size = (value.try(:size) || 0)
-    minimum = options[:minimum]
-    return if name_size >= minimum
-    record.errors.add(attribute, :too_short_name, count: minimum)
-  end
-end
