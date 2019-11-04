78d766f8e013394df4db74616c97476f523e38ab
Kasia <kasia.ekiert@gmail.com>
2019-11-04 17:42:50 +0100

    proper validator

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,6 @@ class Event < ApplicationRecord
   belongs_to :place 
   belongs_to :user
   has_and_belongs_to_many :members, class_name: "User"
+
+  validates :name, name_lenght: { minimum: 5 }
 end
