ed72c3eac038c66244bb3e11ed09ec732b20bd15
Kasia <kasia.ekiert@gmail.com>
2019-11-27 13:55:23 +0100

    change in controller-events_controller - spealling error

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -53,7 +53,7 @@ class EventsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_places
-      @place = Place.all
+      @places = Place.all
     end
 
     def set_event
