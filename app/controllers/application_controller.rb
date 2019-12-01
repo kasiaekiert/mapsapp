7a620ade5e73f81661d5c1bd499e40adbc2dbe3e
Kasia <kasia.ekiert@gmail.com>
2019-12-01 17:59:44 +0100

    remove me from event - add members controller, application controller, routes - to check and fix in the future

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,9 @@ class ApplicationController < ActionController::Base
   around_action :switch_locale
   rescue_from CanCan::AccessDenied, with: :unauthorized
 
-  def switch_locale(&action)     
+  def switch_locale(&action)
     locale = current_user ? current_user.setting.locale : I18n.default_locale
-  
-    I18n.with_locale(locale, &action)   
+    I18n.with_locale(locale, &action)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :places
   resources :events do
-    resource :members, only: [:create, :destroy]
+    resources :members, only: [:create, :destroy]
+    resources :profile, only: [:delete]
   end
   resources :settings, only: [:update]
 
   get "profile", to: "profile#show"
-  
   root "places#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
