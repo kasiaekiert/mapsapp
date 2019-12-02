43caf9ea74aba36e3e8eb2806870c5a516c7bf75
Kasia <kasia.ekiert@gmail.com>
2019-12-02 13:42:31 +0100

    add in model-event validation for members

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,5 @@ class Event < ApplicationRecord
   has_and_belongs_to_many :members, class_name: "User"
 
   validates :name, name_lenght: { minimum: 5 }
+  validates_uniqueness_of :members, scope: [:member_id]
 end
