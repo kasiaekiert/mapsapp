a653168ada381ebb95a79d9cad3ac881b026b381
Kasia <kasia.ekiert@gmail.com>
2019-12-03 18:20:25 +0100

    nested form for place in event/show

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,7 @@ class Event < ApplicationRecord
   belongs_to :place 
   belongs_to :user
   has_and_belongs_to_many :members, class_name: "User"
+  accepts_nested_attributes_for :place
 
   validates :name, name_lenght: { minimum: 5 }
 
