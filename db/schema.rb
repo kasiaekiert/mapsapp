eb4f1d6d00da4b7e563f4c82f526bb8de16f83fe
Kasia <kasia.ekiert@gmail.com>
2019-12-10 10:07:52 +0100

    add user_id to places - migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_02_161913) do
+ActiveRecord::Schema.define(version: 2019_12_10_090306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2019_12_02_161913) do
     t.text "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "settings", force: :cascade do |t|
