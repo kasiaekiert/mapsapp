039412485c51a2eb38da3b656fa869c9626e0a0c
Kasia <kasia.ekiert@gmail.com>
2019-12-10 14:50:34 +0100

    add button to delete event, owner may delete event, working on ability.rb

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,11 +1,19 @@
 class Event < ApplicationRecord
   belongs_to :place 
   belongs_to :user
-  has_and_belongs_to_many :members, class_name: "User"
+  has_and_belongs_to_many :members, class_name: "User", dependent: :destroy 
 
   validates :name, name_lenght: { minimum: 5 }
 
   def has_member?(user)
     members.include?(user)
+  end
+
+  def before_destroy
+    if event.has_member?
+      flash[:notice] = "There are some members in your event. Are you sure that you want to remove it?"
+    else
+      flash[:notice] = "Are you sure?"
+    end
   end
 end
