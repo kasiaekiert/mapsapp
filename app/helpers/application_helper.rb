92a258c27f97e97b8b2476f8334dc48b929e73eb
Kasia <kasia.ekiert@gmail.com>
2019-12-12 11:25:41 +0100

    issue #27 and #23 missing translation in devise and single qnotes in helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,10 @@ module ApplicationHelper
   def flash_message
     flash.map do |key, value|
       level_class = case key
-        when 'notice' then "alert alert-info"
-        when 'success' then "alert alert-success"
-        when 'error' then "alert alert-danger"
-        when 'alert' then "alert alert-warning"
+        when 'notice' then 'alert alert-info'
+        when 'success' then 'alert alert-success'
+        when 'error' then 'alert alert-danger'
+        when 'alert' then 'alert alert-warning'
       end 
       content_tag :div, class: level_class do
        [ value, button_tag ].join('').html_safe
@@ -15,10 +15,10 @@ module ApplicationHelper
   end
 
   def button_tag
-    content_tag :button, class: "close", 'data-dismiss' => "alert", type: 'button' do
+    content_tag :button, class: 'close', 'data-dismiss' => 'alert', type: 'button' do
       [
-        content_tag(:span, "x", 'aria-hidden' => "true"),
-        content_tag(:span, "Close", class: 'sr-only')
+        content_tag(:span, 'x', 'aria-hidden' => 'true'),
+        content_tag(:span, 'Close', class: 'sr-only')
       ].join('').html_safe
     end
   end
