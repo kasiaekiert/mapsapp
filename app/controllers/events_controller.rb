c542cf955e4be2241897f9da179be20ee714de79
Kasia <kasia.ekiert@gmail.com>
2019-12-13 09:47:37 +0100

    method set_event available only for create and destroy

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  before_action :set_event, only: [:show, :edit, :update, :destroy]
+  before_action :set_event, only: [:create, :destroy]
   before_action :check_authorization, only: [:edit, :update, :destroy]
 
   def index
