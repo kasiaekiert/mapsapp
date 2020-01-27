629ad12842cc2eee323263c9edfc6169f412963f
Kasia <kasia.ekiert@gmail.com>
2020-01-27 18:33:35 +0100

    remove conflicts in members controller file

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -7,12 +7,7 @@ class MembersController < ApplicationController
 
   def destroy
     event = Event.find(params[:event_id])
-<<<<<<< HEAD
     event.members.destroy(current_user)
-=======
-    event.members = current_user
-    redirect_to event_members_path(event)
->>>>>>> removing whitespaces and double qunotes
     redirect_back(fallback_location: root_path)
   end
 end
