43d311da337db9adb203794a77856c92c44bb587
Kasia <kasia.ekiert@gmail.com>
2020-02-03 17:56:48 +0100

    avoid n+1 in event/index page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,7 @@ class EventsController < ApplicationController
   before_action :check_authorization, only: [:edit, :update, :destroy]
 
   def index
-    @events = Event.sort_by_start_time
+    @events = Event.includes(:members).sort_by_start_time
   end
 
   def show
