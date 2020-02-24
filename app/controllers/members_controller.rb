b2533532f1b96356b4dcfcfc79708ae5a0c85016
Kasia <kasia.ekiert@gmail.com>
2020-02-24 12:05:06 +0100

    members-controller add before_action

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,13 +1,21 @@
+# frozen_string_literal: true
+
 class MembersController < ApplicationController
+  before_action :set_event, only: %i[create destroy]
+
   def create
-    event = Event.find(params[:event_id])
     event.members << current_user
     redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    event = Event.find(params[:event_id])
     event.members.destroy(current_user)
     redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def set_event
+    event = Event.find(params[:event_id])
   end
 end
