d489ad66fda8b570d49cb915f43739b951a6a00f
Kasia <kasia.ekiert@gmail.com>
2020-03-05 11:50:12 +0100

    resolved places tests

--- spec/views/places/new.html.erb_spec.rb
+++ spec/views/places/new.html.erb_spec.rb
@@ -1,21 +1,24 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "places/new", type: :view do
+RSpec.describe 'places/new', type: :view do
   before(:each) do
+    user = User.create(email: 'user23@user23.com', password: 'user23@user23.com', password_confirmation: 'user23@user23.com')
     assign(:place, Place.new(
-      :name => "MyString",
-      :address => "MyText"
-    ))
+                     name: 'MyString',
+                     address: 'MyText',
+                     creator: user
+                   ))
   end
 
-  it "renders new place form" do
+  it 'renders new place form' do
     render
 
-    assert_select "form[action=?][method=?]", places_path, "post" do
+    assert_select 'form[action=?][method=?]', places_path, 'post' do
+      assert_select 'input[name=?]', 'place[name]'
 
-      assert_select "input[name=?]", "place[name]"
-
-      assert_select "textarea[name=?]", "place[address]"
+      assert_select 'input[name=?]', 'place[address]'
     end
   end
 end
